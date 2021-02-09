f1c63ea926043605610bbdbf5885d13e6bce71e4
krasniukk <tania.krasniuk@gmail.com>
2021-02-09 17:41:47 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Scyscrapers',
+   version='1.0',
+   description='A module for checking if a board for the game "Skyscrapers" is winning or not',
+   author='Krasniukk',
+   author_email='tetiana.krasniuk@ucu.edu.ua',
+   packages=['Skyscrapers'],  #same as name
+   install_requires=''
+)
